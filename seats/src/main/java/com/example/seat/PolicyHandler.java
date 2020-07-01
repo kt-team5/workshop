4a151870090730e90d0997014852acb45018168d
mimdong0917 <63759370+mimdong0917@users.noreply.github.com>
2020-07-01 19:32:50 +0900

    Update PolicyHandler.java

--- seats/src/main/java/com/example/seat/PolicyHandler.java
+++ seats/src/main/java/com/example/seat/PolicyHandler.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PolicyHandler {
 
 	@Autowired
@@ -14,21 +16,19 @@ public class PolicyHandler {
 	public void onEventByString(@Payload PaymentPlaced paymentPlaced) {
 		if(paymentPlaced.getEventType().contentEquals("PaymentPlaced")) {
 			
-			//Optional<Seat> seatById = seatRepository.findById(paymentPlaced.getSeatId());
-			
 			System.out.println("===============");
 			System.out.println("자리 예약 및 결제 완료");
-			
+
 			Seat seat = new Seat();
 			seat.setSeatId(paymentPlaced.getSeatId());
 			seat.setStartTime(paymentPlaced.getStartTime());
 			seat.setUsage(paymentPlaced.getUsage());
 			seat.setOccupied(true);
-			
 			seatRepository.save(seat);
+
 			System.out.println("===============");
-			
-		}
+
 		
+		}
 	}
 }
