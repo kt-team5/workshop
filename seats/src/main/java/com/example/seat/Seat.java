ad9191cdf002430b26b82ffddcc8627485524a7b
mimdong0917 <63759370+mimdong0917@users.noreply.github.com>
2020-07-01 19:33:53 +0900

    @GeneratedValue 삭제 및 @PreUpdate로 수정

--- seats/src/main/java/com/example/seat/Seat.java
+++ seats/src/main/java/com/example/seat/Seat.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.PostPersist;
+import javax.persistence.PostUpdate;
 
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.MessageChannel;
@@ -19,8 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Entity
 public class Seat {
 
-//	@Id @GeneratedValue
-//    Long id;
+	@Id
     int seatId;
     Date startTime;
     int usage;
@@ -28,42 +28,49 @@ public class Seat {
     
     
    // 사용 중지 버튼을 클릭
-    @PostPersist  
+    @PostUpdate  
     public void useStopped() {
-    	
-    	System.out.println("사용 중지 버튼 클릭 event 발생");
-    	
+    	System.out.println("이벤트탈까요 안탈까요");
+    
     	SeatFinished seatFinished = new SeatFinished();
-//    	seatFinished.setId(this.getId());
-    	seatFinished.setSeatId(this.getSeatId());
-    	seatFinished.setOccupied(this.isOccupied());
-    	    	
-    	ObjectMapper objectMapper = new ObjectMapper();
-    	String json = null;
+
+
+    	if(this.isOccupied() == false) {
+    		// 사용 중지 버튼 클릭 event 발생
+
+    		System.out.println("사용 중지 버튼 클릭 event 발생");
+    		
+    		seatFinished.setSeatId(this.getSeatId());
+    		seatFinished.setUsage(this.getUsage());
+    		seatFinished.setOccupied(this.isOccupied());
+    		
+    		ObjectMapper objectMapper = new ObjectMapper();
+    		String json = null;
+    		
+    		try {
+    			json = objectMapper.writeValueAsString(seatFinished);
+    		} catch (JsonProcessingException e) {
+    			throw new RuntimeException("JSON format exception", e);
+    		}
+    		
+    		Processor processor = SeatApplication.applicationContext.getBean(Processor.class);
+    		MessageChannel outputChannel = processor.output();
+    		
+    		outputChannel.send(MessageBuilder
+    				.withPayload(json)
+    				.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+    				.build());
+    		
+    		System.out.print("Seat class:"+json);
+
+    	}else {
+    		System.out.println("좌석 occupied 값"+this.isOccupied());
+    	}
     	
-        try {
-            json = objectMapper.writeValueAsString(seatFinished);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("JSON format exception", e);
-        }
-
-        Processor processor = SeatApplication.applicationContext.getBean(Processor.class);
-        MessageChannel outputChannel = processor.output();
-
-        outputChannel.send(MessageBuilder
-                .withPayload(json)
-                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
-                .build());
     
     }
 
 
-//	public Long getId() {
-//		return id;
-//	}
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
 	public boolean isOccupied() {
 		return occupied;
 	}
