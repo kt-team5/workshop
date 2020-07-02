5a00358f9a1a7f20047e1b66a115b2abb9564867
sennes2 <36391228+sennes2@users.noreply.github.com>
2020-07-02 16:09:29 +0900

    timer 추가

--- seat/src/main/java/com/example/seat/Seat.java
+++ seat/src/main/java/com/example/seat/Seat.java
@@ -1,6 +1,7 @@
 package com.example.seat;
 
 import java.util.Date;
+import java.util.Optional;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -25,46 +26,79 @@ public class Seat {
 	@Id
     int seatId;
     Date startTime;
-    int usage;
+    int usages;
     boolean occupied;
     
    // 사용 중지 버튼을 클릭
     @PostUpdate  
-    public void useStopped() {    
-    	SeatFinished seatFinished = new SeatFinished();
-
+    public void useStopped() {
+    	System.out.println("이벤트탈까요 안탈까요");
+    	
+    	if(this.isOccupied() == false) {
     		// 사용 중지 버튼 클릭 event 발생
+    		// this 좌석을 데이터 테이블에서 삭제하도록...
+			SeatRepository seatRepository = SeatApplication.applicationContext.getBean(SeatRepository.class);
+	    	seatRepository.deleteById(this.getSeatId());
 
     		System.out.println("사용 중지 버튼 클릭 event 발생");
+    		// this 좌석을 삭제하는 않게 : original
+//    		SeatFinished seatFinished = new SeatFinished();
+//
+//    		seatFinished.setSeatId(this.getSeatId());
+//    		seatFinished.setUsages(0);				// 초기화
+//    		seatFinished.setStartTime(null);		// 초기화
     		
-    		seatFinished.setSeatId(this.getSeatId());
-    		seatFinished.setUsage(0);				// 초기화
-    		seatFinished.setStartTime(null);		// 초기화
-    		seatFinished.setOccupied(this.isOccupied());
     		
-    		ObjectMapper objectMapper = new ObjectMapper();
-    		String json = null;
-    		
-    		try {
-    			json = objectMapper.writeValueAsString(seatFinished);
-    		} catch (JsonProcessingException e) {
-    			throw new RuntimeException("JSON format exception", e);
-    		}
-    		
-    		Processor processor = SeatApplication.applicationContext.getBean(Processor.class);
-    		MessageChannel outputChannel = processor.output();
-    		
-    		outputChannel.send(MessageBuilder
-    				.withPayload(json)
-    				.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
-    				.build());
-    		
-    		System.out.print("Seat class:"+json);
-   	
-    
+    		// SeatFinished 발생하지 않도록 함
+//    		ObjectMapper objectMapper = new ObjectMapper();
+//    		String json = null;
+//    		
+//    		try {
+//    			json = objectMapper.writeValueAsString(seatFinished);
+//    		} catch (JsonProcessingException e) {
+//    			throw new RuntimeException("JSON format exception", e);
+//    		}
+//    		
+//    		Processor processor = SeatApplication.applicationContext.getBean(Processor.class);
+//    		MessageChannel outputChannel = processor.output();
+//    		
+//    		outputChannel.send(MessageBuilder
+//    				.withPayload(json)
+//    				.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+//    				.build());
+//    		
+//    		System.out.print("Seat class:"+json);
+
+    	}else {
+    		System.out.println("좌석 occupied 값"+this.isOccupied());
+    	}
+  
     }
 
+    // 각 seat의 time expire 여부 체크
+    public void checkTimer() {
+    	
+    	if(isOccupied() == false) {
+    		return;
+    	}
 
+		Date currentTime = new Date();
+		long diff = currentTime.getTime() - getStartTime().getTime();
+		int min = (int)(diff / 60000); // 몇 분이 지났는가
+//		int min = (int)(diff / 1000); // 몇 초 지났는가 : 시험용
+		System.out.println("====="+min+"분 지남");
+		
+		
+		// 사용시간 만료됨
+		if(min >= getUsages()) {
+			System.out.println("====="+min+" 지남"+"사용시간 "+getUsages());
+			SeatRepository seatRepository = SeatApplication.applicationContext.getBean(SeatRepository.class);
+			
+	    	seatRepository.deleteById(this.getSeatId());
+		}
+     	
+    }
+    
 	public boolean isOccupied() {
 		return occupied;
 	}
@@ -83,12 +117,12 @@ public class Seat {
 	public void setStartTime(Date startTime) {
 		this.startTime = startTime;
 	}
-	public int getUsage() {
-		return usage;
+	public int getUsages() {
+		return usages;
 	}
 
-	public void setUsage(int usage) {
-		this.usage = usage;
+	public void setUsages(int usages) {
+		this.usages = usages;
 	}
     
 
