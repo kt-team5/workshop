0b48caeab1f988be697dc0672c0dfb08dfc381b4
mimdong0917 <63759370+mimdong0917@users.noreply.github.com>
2020-07-02 09:50:06 +0900

    Long -> Integer

--- seats/src/main/java/com/example/seat/SeatRepository.java
+++ seats/src/main/java/com/example/seat/SeatRepository.java
@@ -2,6 +2,6 @@ package com.example.seat;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface SeatRepository extends PagingAndSortingRepository<Seat, Long> {
+public interface SeatRepository extends PagingAndSortingRepository<Seat, Integer> {
 
 }
