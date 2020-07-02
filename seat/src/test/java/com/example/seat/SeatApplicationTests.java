c1ffd838f821e89a1bf6abc2c8af35a605630043
euisoo413 <kim.euisoo.kt@gmail.com>
2020-07-02 11:39:15 +0900

    1.1

--- seat/src/test/java/com/example/seat/SeatApplicationTests.java
+++ seat/src/test/java/com/example/seat/SeatApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.seat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SeatApplicationTests {
 
 	@Test
