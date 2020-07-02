14d02d27842780b696d0398a50b00c2d8397ecd0
sennes2 <36391228+sennes2@users.noreply.github.com>
2020-07-02 16:12:05 +0900

    timer 추가

--- seat/src/main/java/com/example/seat/SeatApplication.java
+++ seat/src/main/java/com/example/seat/SeatApplication.java
@@ -1,6 +1,8 @@
 package com.example.seat;
 
 
+import java.util.Iterator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -13,9 +15,34 @@ import org.springframework.context.ApplicationContext;
 public class SeatApplication {
 
 	public static ApplicationContext applicationContext;
+	public static long CHECK_PERIOD = 60000; //60초
 	
 	public static void main(String[] args) {
 		applicationContext = SpringApplication.run(SeatApplication.class, args);
+		
+		
+		Thread task = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					//System.out.println("======= Thread.run()");
+					// 각 seat의 time expire 여부 체크
+					SeatRepository seatRepository = applicationContext.getBean(SeatRepository.class);
+					
+					Iterable<Seat> seatList = seatRepository.findAll();
+					for (Iterator<Seat> i = seatList.iterator(); i.hasNext(); ) {
+					    Seat seat = (Seat) i.next();
+					    
+					    seat.checkTimer();
+					}
+					try {
+						Thread.sleep(CHECK_PERIOD);
+					} catch (Exception e) {}
+				}
+			}
+		});
+	
+		task.start();
 	}
 
 	
